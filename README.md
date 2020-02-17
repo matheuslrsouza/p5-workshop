77d72a79596f593d09beee510046c1ffc17c8de2
Matheus Souza <matheuslrsouza@gmail.com>
2020-02-16 22:45:22 -0300

    adiciona exercício 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Workshop de P5JS
 
+
+### Exercício 2
+* Faça com que o círculo se movimente na direção do ângulo $\theta$ informado.
--- sketch.js
+++ sketch.js
@@ -1,14 +1,28 @@
+var posX = 0;
+var posY = 0;
 
+var velX = 0.5;
+var velY = 0;
+
+var theta;
 
 function setup() {
     createCanvas(600, 600);
     background(0);
+
+    // TODO inicializar theta
 }
 
-var posX = 0;
-var posY = 0;
-
 function draw() {
-    
+    var centroX = width / 2;
+    var centroY = height / 2;
+
+    translate(centroX, centroY);
+    circle(posX, posY, 30);
+
+    posX += velX;
+    posY += velY;
+
+    //TODO calcular o deslacamento do objeto baseado no ângulo theta
 }
 
