65cf847965de3d5d8c988f35e71ccb0bad2dce8a
Matheus Souza <matheuslrsouza@gmail.com>
2020-02-18 09:06:24 -0300

    implementacao color dots

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Workshop de P5JS
 
+### Color dots
+
+Pontos coloridos na tela em formato de "flor", com aceleração angular em função do cosseno do ângulo
+<br/><br/>
+<img src="img/color-dots1.png" />
+<img src="img/color-dots2.png" />
+<img src="img/color-dots3.png" />
--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 
+var angulo = 0;
 
 function setup() {
     createCanvas(600, 600);
@@ -8,7 +9,27 @@ function setup() {
 var posX = 0;
 var posY = 0;
 
+var raio = 100;
+var acc = 1;
+
 function draw() {
+
+    translate(width/2, height/2);
+
+    var x = raio * cos(angulo);
+    var y = raio * sin(angulo);
+
+    var distOrigem = Math.sqrt(x * x + y * y);
+
+    var alpha = map(distOrigem, 0, 300, 0, 255);
+
+    noStroke();    
+    fill(random(255), random(255), random(255), alpha);
+    circle(x, y, 20);
     
+    acc = (PI / 180) * abs(cos(angulo/2) * 20);
+    angulo = (angulo + (PI / 180)) + acc;
+
+    raio = abs(cos(angulo*8) * 300);
 }
 
